16d9584635988822f38520fde58faf719884f4bc
Maciej Fender <maciejf@int.pl>
2021-10-19 23:46:04 +0200

    pełna implementacja zadania z singletonem

--- ZTP_PS_1/src/com/company/Main.java
+++ ZTP_PS_1/src/com/company/Main.java
@@ -28,36 +28,41 @@ public class Main {
          */
 
         //dodaanie do bazy przez polaczenie 1
-        System.out.println("dodaanie do bazy przez polaczenie 1.: ");
+        System.out.println("\nDodanie do bazy przez polaczenie 1. wartości: \n");
         for(int i =0; i<10;i++){
             p1.set(i,(char)(i+'a'));
+            System.out.println("  wartość: "+((char)(i+'a'))+"   pod indeksem: "+i);
         }
+        System.out.println("\n\n");
 
-        System.out.println("Sprawdzenie co kryje się przy użyciu innego połączenia z innej bazy");
+        System.out.print("Sprawdzenie co kryje się przy użyciu innego połączenia z innej bazy:  ");
         //sprawdzenie co sie kryje przy uzyciu innego polaczenia z innej bazy
         for(int i =0; i<10;i++){
             System.out.print(p3.get(i));
         }
+        System.out.println("\n");
 
         //zmiana danych przy jeszcze nowo utworzonym polączeniu
-        System.out.println("Zmiana danych przy jeszcze nowo utworzonym połaczeniu: ");
+        System.out.println("Zmiana danych przy jeszcze nowo utworzonym połaczeniu  ");
         IPolaczenie polTestowe1 = Baza.getPolaczenie();
         for(int i = 5; i<10;i++)
         {
-            polTestowe1.set(i,(char)('1'+i-2));
+            polTestowe1.set(i,(char)('1'+i-2)); // wpisanie danych zmienionych
         }
 
 
+
         //wypisanie danych jeszcze nowo utworzonym polączeniu
-System.out.println("Wypisanie danych na jeszcze nowo utworzonym połączeniu z innej bazy");
+    System.out.print("\nWypisanie danych na jeszcze nowo utworzonym połączeniu z innej bazy:  ");
         IPolaczenie polTestowe2 = Baza.getPolaczenie();
         for(int i = 0; i<10;i++)
         {
             System.out.print(polTestowe1.get(i));
         }
+        System.out.println("\n\n");
 
 
-
+    System.out.println("WNIOSKI: wyświetlając referencje można zaobserwować zmianę wartości i to, że co 3. pobrania instancji wartość jest taka sama. Dodatkowo po tworzeniu wykorzystywaniu połączeń nowoutworzonych można korzystać dalej z jedynej instancji Bazy.");
 
 
 
--- ZTP_PS_1/src/com/company/classes/Baza.java
+++ ZTP_PS_1/src/com/company/classes/Baza.java
@@ -5,15 +5,34 @@ import com.company.Interfaces.IPolaczenie;
 import java.util.ArrayList;
 
 public class Baza {
-    private char[] tab ; /* ... */
+    private char[] tab = new char[100]; /* ... */
 
+    private static Baza instance;
+
+    /**
+     * Statyczna metoda klasy Baza, stworzona w celu umożliwienia pobierania połączeń do bazy.
+     */
     public static IPolaczenie getPolaczenie() {
 
         return Polaczenie.getInstance();
 
     }
 
+    /**
+     * Zablokowanie dostępu do konstruktora z poziomu innych klas
+     */
     private Baza(){}
+
+    /**
+     * Statyczna metoda do pobierania instancji obiektu z Singletonu.
+     * @return Obiekt klasy Baza
+     */
+    public static Baza getInstance(){
+        if(instance==null){
+            instance = new Baza();
+        }
+        return instance;
+    }
 
     private static class Polaczenie implements IPolaczenie {
 
@@ -21,22 +40,34 @@ public class Baza {
 
         private static ArrayList<IPolaczenie> instaces;
 
+        /**
+         * wskazanie na obecny, ostanio wykorzsytany indeks
+         */
         private static Integer counter;
 
-        private Polaczenie(){}
+        /**
+         * prywatny konstruktor. Jego rolę przejeła metoda getInstance
+         */
+        private Polaczenie(){
+            baza = Baza.getInstance();
+        }
 
+        /**
+         * zadeklarowanie ilości połączeń do wykorzystania z puli
+         */
+        private static Integer numberOfConnections = 3; // ilość połączeń w puli
+
+        /**
+         * Statyczna metoda pobierania instancji połączenia i zwrócenia jednego z puli. Wykorzystana została wczesna inicjalizacja.
+         */
         public static IPolaczenie getInstance() {
-
-
 
             if(instaces==null){
                 instaces = new ArrayList<IPolaczenie>();
-                instaces.add(new Polaczenie());
-                instaces.add(new Polaczenie());
-                instaces.add(new Polaczenie());
-                counter = 2;
+                for( int i =0;i<numberOfConnections;i++) instaces.add(new Polaczenie());
+                counter = instaces.size()-1; //ustawienie wartości o jeden mniejszej, by kolejna brana była wartość z indeksu zerowego listy
             }
-            counter = (counter+1)%3;
+            counter = (counter+1)%instaces.size();
             return instaces.get(counter);
         }
 
